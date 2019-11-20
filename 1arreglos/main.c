c69b2d7259523a2979aee8381cc48e6fb58490dd
Jose Saavedra <joseesaavedraa@gmail.com>
2019-11-19 21:51:51 -0500

    Initial Commit

---
+++ 1arreglos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1arreglos
+//
+//  Created by Jose Saavedra on 11/19/19.
+//  Copyright © 2019 Jose Saavedra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
